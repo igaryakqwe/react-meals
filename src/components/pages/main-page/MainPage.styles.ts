1a84383a35adb571fdfc3cf5027e1c3ae0cc4f5f
igaryakqwe <igor.novykov@gmail.com>
2023-05-01 16:04:55 +0300

    replace component from meals to main page

--- src/components/pages/main-page/MainPage.styles.ts
+++ src/components/pages/main-page/MainPage.styles.ts
@@ -1,5 +1,5 @@
-import {SxProps} from "@mui/system";
-import {Theme} from "@mui/material";
+import { SxProps } from "@mui/system";
+import { Theme } from "@mui/material";
 
 const pageWrapper: SxProps<Theme> = {
   display: 'flex',
@@ -7,7 +7,75 @@ const pageWrapper: SxProps<Theme> = {
   flexWrap: 'wrap',
   alignItems: 'center',
   flexDirection: 'column',
+  gap: '20px'
+}
+
+const cardWrapper: SxProps<Theme> = {
+  maxWidth: '1240px',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center',
+  alignItems: 'center',
+  flexWrap: 'wrap',
+  gap: '60px',
+}
+
+const mealCard: SxProps<Theme> = {
+  minWidth: '300px',
+  height: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  gap: '15px',
+  borderRadius: '3%',
+  margin: '0 15px 0 15px',
+  textAlign: 'center',
+  boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px',
+  transition: '0.5s',
+  '&:hover': {
+    boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px',
+  }
+}
+
+const mealCardBlock: SxProps<Theme> = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  flexWrap: 'wrap',
+  margin: '15px 0 15px 0',
+  width: '280px',
+}
+
+const mealCardText: SxProps<Theme> = {
+  header: {
+    fontWeight: 600,
+  },
+  wordBreak: 'break-all',
+  fontFamily: 'Calibri',
+  maxWidth: "280px",
+  height: '100%',
 }
 
 
-export { pageWrapper };
+const mealCardLink: object = {
+  textDecoration: 'none',
+  color: "black",
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+}
+
+const image: object = {
+  width: '300px',
+  height: '300px',
+  borderRadius: '2% 2% 0 0'
+}
+
+const pagination = {
+  marginTop: '20px'
+}
+
+export { pageWrapper, cardWrapper, mealCard, mealCardText, mealCardBlock, mealCardLink, image, pagination };
--- src/components/pages/main-page/MainPage.tsx
+++ src/components/pages/main-page/MainPage.tsx
@@ -1,15 +1,64 @@
-import React, {useState} from 'react';
-import Meals from "@/components/common/meals";
-import { Box } from "@mui/material";
+import React from 'react';
+import {Box, Pagination, Typography} from "@mui/material";
 import * as styles from './MainPage.styles';
+import {useQuery} from "react-query";
+import MealsService from "@/services/meals.service";
+import usePagination from "@/hooks/usePagination";
+import Loader from "@/components/common/loader/Loader";
+import Error from "@/components/common/error";
 
 const MainPage = () => {
 
+  const { data, isLoading, isError } = useQuery(
+    "meals",
+    MealsService.getAllMeals,
+    {
+      refetchOnWindowFocus: false,
+    }
+  );
+
+  const itemsPerPage = 9;
+
+  const { paginatedData, jumpToPage, currentPage, totalPages } =
+    usePagination(data?.meals, itemsPerPage);
+
+  const handlePageChange = (event, page) => jumpToPage(page)
+
+  if (isLoading) return (
+    <Loader />
+  );
+
+  if (isError) return (
+    <Error />
+  );
+
   return (
     <Box sx={styles.pageWrapper}>
-      <Meals />
+      <Box sx={styles.cardWrapper}>
+        {paginatedData?.map((meal) => (
+          <Box sx={styles.mealCard} key={meal.idMeal}>
+            <a href={`/${meal.idMeal}`} style={styles.mealCardLink}>
+              <img src={meal.strMealThumb} alt={meal.strMeal} style={styles.image} />
+              <Box sx={styles.mealCardBlock}>
+                <Typography sx={styles.mealCardText?.header} variant="h5">{meal.strMeal}</Typography>
+                <Typography sx={styles.mealCardText} variant="h6">{meal.strCategory}</Typography>
+                <Typography sx={styles.mealCardText} variant="h6">{meal?.strTags}</Typography>
+              </Box>
+            </a>
+          </Box>
+        ))}
+      </Box>
+      <Box>
+        <Pagination
+          sx={styles.pagination}
+          count={totalPages}
+          page={currentPage}
+          onChange={handlePageChange}
+          size="large"
+        />
+      </Box>
     </Box>
-  )
+  );
 };
 
 export default MainPage;
